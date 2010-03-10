b48d7c26b326b7179e40514d0a30c58a8784e44d
Jeremy Jarrell <jeremy.jarrell@gmail.com>
2010-03-09 19:15:11 -0500

    test coverage for post actions

--- src/Bloggeriffic/Bloggeriffic.Tests/Controllers/IndexController_Specs.cs
+++ src/Bloggeriffic/Bloggeriffic.Tests/Controllers/IndexController_Specs.cs
@@ -9,17 +9,34 @@ namespace Bloggeriffic.Tests.Controllers
     [TestFixture]
     public class IndexController_Specs
     {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void Before_each_spec()
+        {
+            controller = new PostController();
+        }
+
+        #endregion
+
+        private PostController controller;
+
         [Test]
         public void Should_display_First_post()
         {
-            var controller = new PostController();
-            var posts = (ViewResult)controller.Post(1);
+            ViewResult posts = controller.Post(1);
 
             var titles = (string)posts.ViewData["post"];
 
             Assert.IsNotNull(titles);
         }
 
-        
+        [Test]
+        public void Should_return_all_available_posts()
+        {
+            var posts = (ViewResult)controller.Index();
+            var titles = (IEnumerable<string>)posts.ViewData["posts"];
+            Assert.That(titles.Count(), Is.EqualTo(3));
+        }
     }
 }
